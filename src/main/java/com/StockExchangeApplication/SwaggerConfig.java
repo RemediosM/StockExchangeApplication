e6198fa1e2b31d73c24dd7ad9557665646ef425c
Aleksandra Grzeczyńska <puszka004@gmail.com>
2023-02-28 23:02:29 +0100

    Swagger configuration fix

--- src/main/java/com/StockExchangeApplication/SwaggerConfig.java
+++ src/main/java/com/StockExchangeApplication/SwaggerConfig.java
@@ -3,12 +3,11 @@ package com.StockExchangeApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
 @EnableSwagger2
@@ -18,8 +17,8 @@ public class SwaggerConfig {
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.StockExchangeAlert.share"))
-                .paths(regex("/share.*"))
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
                 .build();
     }
 
